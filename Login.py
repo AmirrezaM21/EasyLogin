1daed5ac1b6a7e373bae4fa518c974a2869f0f09
AmirrezaM21 <68157638+AmirrezaM21@users.noreply.github.com>
2020-07-21 15:36:36 +0430

    Add files via upload
    Click on the first (username) field to fill automatically, then you can close it.

---
+++ Login.py
@@ -0,0 +1,21 @@
+# Code to check if left or right mouse buttons were pressed
+import win32api
+import time
+import pyautogui as pag
+state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
+state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
+email = "Your_Email_Here!"
+password = "Your_Password_Here!"
+while True:
+    a = win32api.GetKeyState(0x01)
+    b = win32api.GetKeyState(0x02)
+    if a != state_left:  # Button state changed
+        state_left = a
+        print(a)
+        if a < 0:
+            pag.typewrite(email)
+            time.sleep(0.1)
+            pag.press('tab')
+            time.sleep(0.1)
+            pag.typewrite(password)
+            pag.press('enter')
